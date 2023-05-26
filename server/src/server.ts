101655456268d7661924d28f06d2c2f0a9138ef3
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2023-05-25 22:58:40 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import jwt from "@fastify/jwt";
+import fastify from "fastify";
+import { memoriesRoutes } from "./routes/memories";
+import cors from "@fastify/cors";
+import multipart from "@fastify/multipart";
+import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
+
+const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
+
+app.register(cors, { origin: true });
+
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(memoriesRoutes);
+app.register(authRoutes);
+app.register(uploadRoutes);
+
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("🚀 Server on http://localhost:3333");
+  });
